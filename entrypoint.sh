2ba9e23d2120722fecc5d6e48f57b29d12961010
Dvin <dvyu@yandex.ru>
2022-11-30 13:50:23 +0500

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,25 @@ check_for_kube_file() {
     return 1
   fi
 }
+
+check_and_apply() {
+if check_for_kube_file;then
+  echo check $FILE
+  echo dry run client
+  kubectl apply --dry-run='client' -f $FILE
+  echo dry run server
+  kubectl apply --dry-run='server' -f $FILE
+  if [ "$APPLY" = "true" ];then
+    echo APPLY $FILE
+    kubectl apply -f $FILE
+  fi
+else
+  echo $FILE not valid kube file
+fi
+}
 # run checks
+DELETED_FILES=/tmp/deleted_files
+echo > $DELETED_FILES
 echo get kube config
 echo "$KUBE_CONFIG_DATA" | base64 -d > /tmp/config
 export KUBECONFIG=/tmp/config
@@ -20,9 +38,7 @@ kubectl get nodes
 echo get git changes
 git config --global --add safe.directory /github/workspace
 LAST_COMMIT=$(git --no-pager log | head -1 | awk -F"it " '{print $2}')
-PREV_COMMIT=$(git --no-pager log | grep ^commit | head -2 | tail -1 | awk -F"it " '{print $2}')
 echo LAST_COMMIT=$LAST_COMMIT
-echo PREV_COMMIT=$PREV_COMMIT
 #
 for FILE in $((
            for GID in $(git --no-pager show $LAST_COMMIT | grep ^--- | grep -v /dev/null | awk -F"a/" '{print $2}');do echo $GID; done
@@ -30,21 +46,34 @@ for FILE in $((
            ) | sort | uniq )
 do
   if [ -f $FILE ];then
-    if check_for_kube_file;then
-      echo check $FILE
-      echo dry run client
-      kubectl apply --dry-run='client' -f $FILE
-      echo dry run server
-      kubectl apply --dry-run='server' -f $FILE
-      if [ "$APPLY" = "true" ];then
-        echo APPLY $FILE
-        kubectl apply -f $FILE
-      fi
-    else
-      echo $FILE not valid kube file
-    fi
+    check_and_apply
+#    if check_for_kube_file;then
+#      echo check $FILE
+#      echo dry run client
+#      kubectl apply --dry-run='client' -f $FILE
+#      echo dry run server
+#      kubectl apply --dry-run='server' -f $FILE
+#      if [ "$APPLY" = "true" ];then
+#        echo APPLY $FILE
+#        kubectl apply -f $FILE
+#      fi
+#    else
+#      echo $FILE not valid kube file
+#    fi
   else
     echo file $FILE deleted
+    echo $FILE >> $DELETED_FILES
+  fi
+done
+# 
+echo check for deleted files
+PREV_COMMIT=$(git --no-pager log | grep ^commit | head -2 | tail -1 | awk -F"it " '{print $2}')
+echo PREV_COMMIT=$PREV_COMMIT
+git checkout $PREV_COMMIT
+for FILE in $(cat $DELETED_FILES)
+do
+  if [ -f $FILE ];then
+    check_and_apply
   fi
 done
 rm /tmp/config
