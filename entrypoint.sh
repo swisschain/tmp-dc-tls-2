78621304dafe89f62adbf0ad42e106a32c6473e6
Dvin <dvyu@yandex.ru>
2022-12-13 19:16:24 +0500

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,6 +44,7 @@ fi
 # run checks
 DELETED_FILES=/tmp/deleted_files
 echo > $DELETED_FILES
+rm $DELETED_FILES
 echo get kube config
 echo "$KUBE_CONFIG_DATA" | base64 -d > /tmp/config
 export KUBECONFIG=/tmp/config
@@ -68,18 +69,22 @@ do
   fi
 done
 # 
-echo check for deleted files
-PREV_COMMIT=$(git --no-pager log | grep ^commit | head -2 | tail -1 | sed 's#commit ##g')
-echo PREV_COMMIT=$PREV_COMMIT
-git checkout $PREV_COMMIT > /dev/null 2>&1
-for FILE in $(cat $DELETED_FILES)
-do
-  echo -=[ processing $FILE ]=-
-  if [ -f $FILE ];then
-    check_and_delete
-  else
-    echo can not find $FILE
-  fi
-done
-rm /tmp/deleted_files
-rm /tmp/config
+if [ -f "$DELETED_FILES" ];then
+  echo check for deleted files
+  PREV_COMMIT=$(git --no-pager log | grep ^commit | head -2 | tail -1 | sed 's#commit ##g')
+  echo PREV_COMMIT=$PREV_COMMIT
+  git checkout $PREV_COMMIT > /dev/null 2>&1
+  for FILE in $(cat $DELETED_FILES)
+  do
+    echo -=[ processing $FILE ]=-
+    if [ -f $FILE ];then
+      check_and_delete
+    else
+      echo can not find $FILE
+    fi
+  done
+  rm $DELETED_FILES
+else
+  echo check for deleted files skipped
+fi
+rm $KUBECONFIG
