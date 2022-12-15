9a16c911d769a245893c817b507e36d566918db4
Dvin <dvyu@yandex.ru>
2022-12-15 19:43:01 +0500

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,3 +7,4 @@ gh repo view
 echo github.event.number=${{ github.event.number }}
 gh api -H "Accept: application/vnd.github+json" /repos/swisschain/tmp-dc-tls/pulls
 #- run: gh api -H "Accept: application/vnd.github+json" /repos/${{ github.repository_owner }}/${{ github.event.repository.name }}/pulls
+
