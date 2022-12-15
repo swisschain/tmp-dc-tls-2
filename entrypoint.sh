7b8cf3d3ff8763fc5934802f850e096a7aec7c6b
Dvin <dvyu@yandex.ru>
2022-12-15 19:54:10 +0500

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,7 @@ set -e
 git config --global --add safe.directory /github/workspace
 gh repo view
 echo GITHUB_EVENT_NUMBER=$GITHUB_EVENT_NUMBER
+echo GITHUB_REPOSITORY_OWNER=$GITHUB_REPOSITORY_OWNER
+echo GITHUB_REPOSITORY_NAME=$GITHUB_REPOSITORY_NAME
 gh api -H "Accept: application/vnd.github+json" /repos/swisschain/tmp-dc-tls/pulls
 #- run: gh api -H "Accept: application/vnd.github+json" /repos/${{ github.repository_owner }}/${{ github.event.repository.name }}/pulls
