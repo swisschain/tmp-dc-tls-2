bf8e448991e860cb71dd851cca389fe001c1f229
Dvin <dvyu@yandex.ru>
2022-12-16 14:59:59 +0500

    entrypoint.sh

--- entrypoint.py
+++ entrypoint.py
@@ -2,7 +2,7 @@
 
 import os
 
-cmd = "gh api -H "Accept: application/vnd.github+json" /repos/$GITHUB_REPOSITORY_OWNER/$GITHUB_REPOSITORY_NAME/pulls/$GITHUB_EVENT_NUMBER | jq .body"
+cmd = "gh api -H \"Accept: application/vnd.github+json\" /repos/$GITHUB_REPOSITORY_OWNER/$GITHUB_REPOSITORY_NAME/pulls/$GITHUB_EVENT_NUMBER | jq .body"
 
 returned_value = os.system(cmd)
 print('returned value:', returned_value)
