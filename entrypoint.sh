694dbba0cfdf7fdda6f1dbb5f9e53082d0951725
Dvin <dvyu@yandex.ru>
2022-12-16 10:53:23 +0500

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,9 @@
 set -e
 
 git config --global --add safe.directory /github/workspace
-gh repo view
 echo GITHUB_EVENT_NUMBER=$GITHUB_EVENT_NUMBER
 echo GITHUB_REPOSITORY_OWNER=$GITHUB_REPOSITORY_OWNER
 echo GITHUB_REPOSITORY_NAME=$GITHUB_REPOSITORY_NAME
-#gh api -H "Accept: application/vnd.github+json" /repos/swisschain/tmp-dc-tls/pulls
-#- run: gh api -H "Accept: application/vnd.github+json" /repos/${{ github.repository_owner }}/${{ github.event.repository.name }}/pulls
-#bash-3.2$ cat /tmp/www | jq '.[] | select(.number == '14') | .body' | sed 's#\\r\\n#\n#g'
-#PR_BODY=$(gh api -H "Accept: application/vnd.github+json" /repos/swisschain/tmp-dc-tls/pulls | jq '.[] | select(.number == $GITHUB_EVENT_NUMBER) | .body')
 PR_BODY=$(gh api -H "Accept: application/vnd.github+json" /repos/$GITHUB_REPOSITORY_OWNER/$GITHUB_REPOSITORY_NAME/pulls/$GITHUB_EVENT_NUMBER | jq .body)
 
 if echo ${PR_BODY} | grep ~deployment-order > /dev/null 2>&1;then
@@ -20,10 +15,11 @@ if echo ${PR_BODY} | grep ~deployment-order > /dev/null 2>&1;then
   echo PR_BODY_STR_COUNT=$PR_BODY_STR_COUNT
   for SI in $(echo ${PR_BODY} | sed 's#\\r\\n#\n#g' | grep -A $PR_BODY_STR_COUNT ~deployment-order)
   do
-    echo $SI
-    if [ "$SI" = ^$ ];then
-      echo epty string found
+    if echo $SI | sed 's/^[[:space:]]*$/space/g'|grep space > /dev/null 2>&1;then
+      #echo epty string found
+      break
     fi
+    echo $SI
   done
 else
   echo ND
