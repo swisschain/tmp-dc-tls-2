e54f5edbd7be6fdf73c868b6de2140a778778813
Dvin <dvyu@yandex.ru>
2022-12-16 16:59:16 +0500

    entrypoint.sh

--- entrypoint.py
+++ entrypoint.py
@@ -5,15 +5,20 @@ import subprocess
 
 cmd1 = "gh api -H \"Accept: application/vnd.github+json\" /repos/$GITHUB_REPOSITORY_OWNER/$GITHUB_REPOSITORY_NAME/pulls/$GITHUB_EVENT_NUMBER | jq .body > /tmp/pr_body"
 #cmd1 = "gh api -H \"Accept: application/vnd.github+json\" /repos/$GITHUB_REPOSITORY_OWNER/$GITHUB_REPOSITORY_NAME/pulls/$GITHUB_EVENT_NUMBER | jq .body | sed 's/\\r\\n/\n/g' > /tmp/pr_body"
-#cmd2 = "/usr/local/bin/gh api -H \"Accept: application/vnd.github+json\" /repos/$GITHUB_REPOSITORY_OWNER/$GITHUB_REPOSITORY_NAME/pulls/$GITHUB_EVENT_NUMBER"
+cmd2 = "/usr/local/bin/gh api -H \"Accept: application/vnd.github+json\" /repos/$GITHUB_REPOSITORY_OWNER/$GITHUB_REPOSITORY_NAME/pulls/$GITHUB_EVENT_NUMBER"
 
-returned_value = os.system(cmd1)
+#returned_value = os.system(cmd1)
 #returned_output = subprocess.check_output(cmd2)
-print('returned value:', returned_value)
+#print('returned value:', returned_value)
 #print('returned output:', returned_output)
 
-pr_body = open('/tmp/pr_body', 'r')
-pr_body_strings = pr_body.readlines()
+#pr_body = open('/tmp/pr_body', 'r')
+#pr_body_strings = pr_body.readlines()
 
-for line in pr_body_strings:
-  print("Line: {}".format(line.strip()))
+#for line in pr_body_strings:
+#  print("Line: {}".format(line.strip()))
+
+p = subprocess.Popen(cmd2, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+for line in p.stdout.readlines():
+    print line,
+retval = p.wait()
