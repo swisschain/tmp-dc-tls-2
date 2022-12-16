073a299a34d92292a6a7e4289ebfcdcc3eba7068
Dvin <dvyu@yandex.ru>
2022-12-16 16:39:49 +0500

    entrypoint.sh

--- entrypoint.py
+++ entrypoint.py
@@ -3,7 +3,7 @@
 import os
 import subprocess
 
-cmd1 = "gh api -H \"Accept: application/vnd.github+json\" /repos/$GITHUB_REPOSITORY_OWNER/$GITHUB_REPOSITORY_NAME/pulls/$GITHUB_EVENT_NUMBER | jq .body | sed 's#\\r\\n#\n#g' > /tmp/pr_body"
+cmd1 = "gh api -H \"Accept: application/vnd.github+json\" /repos/$GITHUB_REPOSITORY_OWNER/$GITHUB_REPOSITORY_NAME/pulls/$GITHUB_EVENT_NUMBER | jq .body | sed 's/\\r\\n/\n/g' > /tmp/pr_body"
 #cmd2 = "/usr/local/bin/gh api -H \"Accept: application/vnd.github+json\" /repos/$GITHUB_REPOSITORY_OWNER/$GITHUB_REPOSITORY_NAME/pulls/$GITHUB_EVENT_NUMBER"
 
 returned_value = os.system(cmd1)
