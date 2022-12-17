544ba7f9395c231e8bb3d7bcfe2f8698dc45f003
Dvin <dvyu@yandex.ru>
2022-12-17 11:40:05 +0500

    entrypoint.sh

--- entrypoint.py
+++ entrypoint.py
@@ -4,32 +4,36 @@ import os
 import subprocess
 import json
 
-cmd1 = "gh api -H \"Accept: application/vnd.github+json\" /repos/$GITHUB_REPOSITORY_OWNER/$GITHUB_REPOSITORY_NAME/pulls/$GITHUB_EVENT_NUMBER | jq .body > /tmp/pr_body"
+#cmd1 = "gh api -H \"Accept: application/vnd.github+json\" /repos/$GITHUB_REPOSITORY_OWNER/$GITHUB_REPOSITORY_NAME/pulls/$GITHUB_EVENT_NUMBER | jq .body > /tmp/pr_body"
 #cmd1 = "gh api -H \"Accept: application/vnd.github+json\" /repos/$GITHUB_REPOSITORY_OWNER/$GITHUB_REPOSITORY_NAME/pulls/$GITHUB_EVENT_NUMBER | jq .body | sed 's/\\r\\n/\n/g' > /tmp/pr_body"
-cmd2 = "/usr/local/bin/gh api -H \"Accept: application/vnd.github+json\" /repos/$GITHUB_REPOSITORY_OWNER/$GITHUB_REPOSITORY_NAME/pulls/$GITHUB_EVENT_NUMBER"
+cmd = "gh api -H \"Accept: application/vnd.github+json\" /repos/$GITHUB_REPOSITORY_OWNER/$GITHUB_REPOSITORY_NAME/pulls/$GITHUB_EVENT_NUMBER"
 
 #returned_value = os.system(cmd1)
 #returned_output = subprocess.check_output(cmd2)
 #print('returned value:', returned_value)
 #print('returned output:', returned_output)
 
-#pr_body = open('/tmp/pr_body', 'r')
-#pr_body_strings = pr_body.readlines()
+count=0
+deployment_order_list = {}
+deployment_order = open('deployment-order-group-priorities', 'r')
+deployment_order_strings = deployment_order.readlines()
+for line in deployment_order_strings:
+  count+=1
+  deployment_order_list{"line"}=count
+  print("Line{}: {} {}".format(count, line.strip(), deployment_order_list{"line"}))
 
-#for line in pr_body_strings:
-#  print("Line: {}".format(line.strip()))
-
-cmd_pipe = subprocess.Popen(cmd2, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+cmd_pipe = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 for gh_response_line in cmd_pipe.stdout.readlines():
     gh_response_line_json = json.loads(gh_response_line)
     #print(gh_response_line_json["body"]) 
     gh_response_comments=gh_response_line_json["body"].split("\r\n")
 
+found_deployment_order=0
 for pr_comment_line in gh_response_comments:
     print(pr_comment_line)
-    if "~" in pr_comment_line:
+    if "~deployment-order" in pr_comment_line:
         print("found1")
-    if pr_comment_line == None:
-        print("empty")
-    if pr_comment_line == '':
+        found_deployment_order=1
+    if pr_comment_line == '' and found_deployment_order == 1:
         print("empty2")
+        break
