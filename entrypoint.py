254e29bd4b831297bf3c785242d80b6b33780e14
Dvin <dvyu@yandex.ru>
2022-12-19 10:06:49 +0500

    entrypoint.sh

--- entrypoint.py
+++ entrypoint.py
@@ -4,26 +4,12 @@ import os
 import subprocess
 import json
 
-#cmd1 = "gh api -H \"Accept: application/vnd.github+json\" /repos/$GITHUB_REPOSITORY_OWNER/$GITHUB_REPOSITORY_NAME/pulls/$GITHUB_EVENT_NUMBER | jq .body > /tmp/pr_body"
-#cmd1 = "gh api -H \"Accept: application/vnd.github+json\" /repos/$GITHUB_REPOSITORY_OWNER/$GITHUB_REPOSITORY_NAME/pulls/$GITHUB_EVENT_NUMBER | jq .body | sed 's/\\r\\n/\n/g' > /tmp/pr_body"
 cmd = "gh api -H \"Accept: application/vnd.github+json\" /repos/$GITHUB_REPOSITORY_OWNER/$GITHUB_REPOSITORY_NAME/pulls/$GITHUB_EVENT_NUMBER"
 
 #returned_value = os.system(cmd1)
 #returned_output = subprocess.check_output(cmd2)
 #print('returned value:', returned_value)
 #print('returned output:', returned_output)
-
-print("read group file...")
-count=0
-deployment_order_list = {}
-deployment_order = open('deployment-order-group-priorities', 'r')
-deployment_order_strings = deployment_order.readlines()
-for line in deployment_order_strings:
-  count+=1
-  deployment_order_list["line"]=count
-  print(count)
-  print(line.strip())
-  print(deployment_order_list["line"])
 
 print("get github pr comment...")
 found_pr_body=0
@@ -42,9 +28,10 @@ for gh_response_line in cmd_pipe.stdout.readlines():
     print('found_pr_body')
     print(found_pr_body)
 
-print("parse comment...")
-found_deployment_order=0
-for pr_comment_line in gh_response_comments:
+if found_pr_body:
+  print("parse comment...")
+  found_deployment_order=0
+  for pr_comment_line in gh_response_comments:
     print(pr_comment_line)
     if "~deployment-order" in pr_comment_line:
         print("found1")
@@ -52,3 +39,16 @@ for pr_comment_line in gh_response_comments:
     if pr_comment_line == '' and found_deployment_order == 1:
         print("empty2")
         break
+else
+  print("comment not found...")
+  print("read group file...")
+  count=0
+  deployment_order_list = {}
+  deployment_order = open('deployment-order-group-priorities', 'r')
+  deployment_order_strings = deployment_order.readlines()
+  for line in deployment_order_strings:
+    count+=1
+    deployment_order_list["line"]=count
+    print(count)
+    print(line.strip())
+    print(deployment_order_list["line"])
