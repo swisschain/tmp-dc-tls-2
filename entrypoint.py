cf851417bca4367377c084bd4500cb19725359f2
Dvin <dvyu@yandex.ru>
2022-12-19 17:28:21 +0500

    entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -9,10 +9,10 @@ LOG = "INFO"
 gh_cmd = "gh api -H \"Accept: application/vnd.github+json\" /repos/$GITHUB_REPOSITORY_OWNER/$GITHUB_REPOSITORY_NAME/pulls/$GITHUB_EVENT_NUMBER"
 #kube_cmd_debug = "echo KUBE_CONFIG_DATA=$KUBE_CONFIG_DATA && echo $KUBE_CONFIG_DATA | base64 -d > /tmp/config && cat /tmp/config && export KUBECONFIG=/tmp/config && set | grep KUBECONFIG  && kubectl get nodes"
 #kube_cmd_info = "echo $KUBE_CONFIG_DATA | base64 -d > /tmp/config"
-kube_cmd1 = "mkdir ~/.kube"
-kube_cmd2 = "echo $KUBE_CONFIG_DATA | base64 -d > ~/.kube/config"
-#kube_cmd3 = "cat ~/.kube/config"
-kube_cmd3 = "kubectl get nodes"
+kube_cmd_dir = "mkdir ~/.kube"
+kube_cmd_config = "echo $KUBE_CONFIG_DATA | base64 -d > ~/.kube/config"
+kube_cmd_config_debug = "cat ~/.kube/config"
+kube_cmd_nodes = "kubectl get nodes"
 
 print("get github pr comment...")
 found_pr_body=0
@@ -29,11 +29,20 @@ for gh_response_line in cmd_pipe.stdout.readlines():
         gh_response_comments=gh_response_line_json["body"].split("\r\n")
     print('found_pr_body:', found_pr_body)
 
+deployment_order_names = {}
 if found_pr_body:
   print("parse comment...")
   found_deployment_order=0
+  count=0
   for pr_comment_line in gh_response_comments:
     print('pr_comment_line:', pr_comment_line)
+    if found_deployment_order == 1:
+      count+=1
+      print("add to deployment_order_names count={}, line={}, check_count={}'.format(count, line.strip(), deployment_order_names["line"]))
+      deployment_order_names["line"]=count
+      #print('count:', count)
+      #print('line:', line.strip())
+      #print('deployment_order_names["line"]:', deployment_order_names["line"])
     if "~deployment-order" in pr_comment_line:
         print("deployment-order detected...")
         found_deployment_order=1
@@ -44,21 +53,24 @@ else:
   print("comment not found...")
   print("read group file...")
   count=0
-  deployment_order_list = {}
   deployment_order = open('deployment-order-group-priorities', 'r')
   deployment_order_strings = deployment_order.readlines()
   for line in deployment_order_strings:
     count+=1
-    deployment_order_list["line"]=count
+    deployment_order_names["line"]=count
     print('count:', count)
     print('line:', line.strip())
-    print('deployment_order_list["line"]:', deployment_order_list["line"])
+    print('deployment_order_names["line"]:', deployment_order_names["line"])
 
 print("get kube config...")
-kube_cmd1_returned_value = os.system(kube_cmd1)
-print('kube_cmd1_returned_value:', kube_cmd1_returned_value)
-kube_cmd2_returned_value = os.system(kube_cmd2)
-print('kube_cmd2_returned_value:', kube_cmd2_returned_value)
-cmd_pipe = subprocess.Popen(kube_cmd3, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+kube_cmd_dir_returned_value = os.system(kube_cmd_dir)
+print('kube_cmd_dir_returned_value:', kube_cmd_dir_returned_value)
+kube_cmd_config_returned_value = os.system(kube_cmd_config)
+print('kube_cmd_config_returned_value:', kube_cmd_config_returned_value)
+if LOG == 'DEBUG':
+  cmd_pipe = subprocess.Popen(kube_cmd_config_debug, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+  for kubectl_response_line in cmd_pipe.stdout.readlines():
+    print('kubectl_response_line:', kubectl_response_line)
+cmd_pipe = subprocess.Popen(kube_cmd_nodes, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 for kubectl_response_line in cmd_pipe.stdout.readlines():
   print('kubectl_response_line:', kubectl_response_line)
