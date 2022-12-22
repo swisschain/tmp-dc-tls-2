7a2f57d395dc561a64f489cd7c4b6899529ff5dc
Dvin <dvyu@yandex.ru>
2022-12-22 22:52:15 +0500

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,6 +41,34 @@ else
   echo $FILE not valid kube file
 fi
 }
+
+get_github_event() {
+ATTEMPTS=$2
+CURRENT_HEAD=$1
+
+while [ "$ATTEMPTS" -gt 0 ]
+do
+  echo ATTEMPTS=$ATTEMPTS 1>&2
+  GITHUB_EVENT_RESPONSE=$(gh api -H "Accept: application/vnd.github+json" /repos/$GITHUB_REPOSITORY_OWNER/$GITHUB_REPOSITORY_NAME/events | jq .[0])
+  GITHUB_EVENT_HEAD=$(echo $GITHUB_EVENT_RESPONSE | jq -r .payload.head)
+  echo GITHUB_EVENT_HEAD=$GITHUB_EVENT_HEAD 1>&2
+  echo CURRENT_HEAD=$CURRENT_HEAD 1>&2
+  if [ "$GITHUB_EVENT_HEAD" = "$CURRENT_HEAD" ];then
+    echo git head and github event head matched 1>&2
+    echo $GITHUB_EVENT_RESPONSE
+    break
+  else
+    echo try again 1>&2
+  fi
+  if [ "$ATTEMPTS" -eq 1 ];then
+    echo can not get right github event EXIT WTITH ERROR 1>&2
+    exit 1
+  fi
+  ATTEMPTS=$(( $ATTEMPTS - 1 ))
+  sleep 3
+done
+}
+
 # run checks
 DELETED_FILES=/tmp/deleted_files
 echo > $DELETED_FILES
@@ -52,7 +80,10 @@ echo get kube nodes
 kubectl get nodes
 echo get git changes
 git config --global --add safe.directory /github/workspace
-GITHUB_EVENT=$(gh api -H "Accept: application/vnd.github+json" /repos/$GITHUB_REPOSITORY_OWNER/$GITHUB_REPOSITORY_NAME/events | jq .[0])
+GIT_HEAD=$(git rev-parse HEAD)
+echo GIT_HEAD=$GIT_HEAD
+#GITHUB_EVENT=$(gh api -H "Accept: application/vnd.github+json" /repos/$GITHUB_REPOSITORY_OWNER/$GITHUB_REPOSITORY_NAME/events | jq .[0])
+GITHUB_EVENT=$(get_github_event $GIT_HEAD 3)
 echo GITHUB_EVENT=$GITHUB_EVENT
 LAST_COMMIT=$(echo $GITHUB_EVENT | jq -r .payload.head)
 echo LAST_COMMIT=$LAST_COMMIT
