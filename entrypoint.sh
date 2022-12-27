7920fd6cf398b45d51f8b0eacd790b9ca8b9685b
Dvin <dvyu@yandex.ru>
2022-12-27 15:51:34 +0500

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -42,33 +42,6 @@ else
 fi
 }
 
-get_github_event() {
-ATTEMPTS=$2
-CURRENT_HEAD=$1
-
-while [ "$ATTEMPTS" -gt 0 ]
-do
-  echo ATTEMPTS=$ATTEMPTS 1>&2
-  GITHUB_EVENT_RESPONSE=$(gh api -H "Accept: application/vnd.github+json" /repos/$GITHUB_REPOSITORY_OWNER/$GITHUB_REPOSITORY_NAME/events | jq .[0])
-  GITHUB_EVENT_HEAD=$(echo $GITHUB_EVENT_RESPONSE | jq -r .payload.head)
-  echo GITHUB_EVENT_HEAD=$GITHUB_EVENT_HEAD 1>&2
-  echo CURRENT_HEAD=$CURRENT_HEAD 1>&2
-  if [ "$GITHUB_EVENT_HEAD" = "$CURRENT_HEAD" ];then
-    echo git head and github event head matched 1>&2
-    echo $GITHUB_EVENT_RESPONSE
-    break
-  else
-    echo try again 1>&2
-  fi
-  if [ "$ATTEMPTS" -eq 1 ];then
-    echo can not get right github event EXIT WTITH ERROR 1>&2
-    exit 1
-  fi
-  ATTEMPTS=$(( $ATTEMPTS - 1 ))
-  sleep 3
-done
-}
-
 # run checks
 DELETED_FILES=/tmp/deleted_files
 echo > $DELETED_FILES
@@ -80,18 +53,11 @@ echo get kube nodes
 kubectl get nodes
 echo get git changes
 git config --global --add safe.directory /github/workspace
-#GIT_HEAD=$(git rev-parse HEAD)
-#echo GIT_HEAD=$GIT_HEAD
-#GITHUB_EVENT=$(gh api -H "Accept: application/vnd.github+json" /repos/$GITHUB_REPOSITORY_OWNER/$GITHUB_REPOSITORY_NAME/events | jq .[0])
-#GITHUB_EVENT=$(get_github_event $GIT_HEAD 3)
 echo GITHUB_EVENT=$GITHUB_EVENT
-#echo GITHUB_CONTEXT=$GITHUB_CONTEXT
 LAST_COMMIT=$(echo $GITHUB_EVENT | jq -r .after)
 echo LAST_COMMIT=$LAST_COMMIT
 PREV_COMMIT=$(echo $GITHUB_EVENT | jq -r .before)
 echo PREV_COMMIT=$PREV_COMMIT
-#CURRENT_COMMIT=$(git rev-parse HEAD)
-#echo CURRENT_COMMIT=$CURRENT_COMMIT
 #
 echo found commits...
 echo $GITHUB_EVENT | jq -r '.commits[] | "\"\(.message)\" (\(.id))"'
