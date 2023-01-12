a40a3faebc93980505509b41a1a59ac6de4d6372
Dvin <dvyu@yandex.ru>
2023-01-12 18:14:20 +0500

    entrypoint.py

--- my_kubernetes.py
+++ my_kubernetes.py
@@ -1,8 +1,9 @@
 # Check kubernetes Kind type function
 def is_kube_yaml_valid( file_yaml, kind_types ):
-    for key, value in file_yaml.items():
-      if "kind" in key.lower():
-        print('Kind key:', value)
+    for yaml_key, yaml_value in file_yaml.items():
+      print('yaml_key:', yaml_key)
+      #if "kind" in key.lower():
+      #  print('Kind key:', value)
     for kind_type in kind_types:
         print('kind_type:', kind_type)
     return True 
