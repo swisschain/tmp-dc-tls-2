5f4673a9b787cc8330436b189b452f8ecd1e7ee8
Dvin <dvyu@yandex.ru>
2023-01-12 18:42:15 +0500

    entrypoint.py

--- my_kubernetes.py
+++ my_kubernetes.py
@@ -2,8 +2,12 @@
 def is_kube_yaml_valid(file_yaml, kind_types):
     for yaml_key, yaml_value in file_yaml.items():
         print('yaml_key:', yaml_key)
-        if yaml_key.lower() in kind_types
-            return True
+        if "kind" in yaml_key.lower():
+            print('Found Kind Key with yaml_value:', yaml_value)
+            for kind_type in kind_types:
+                print('kind_type:', kind_type)
+                if kind_type.lower() == yaml_value.lower():
+                    return True
       #if "kind" in key.lower():
       #  print('Kind key:', value)
     #for kind_type in kind_types:
