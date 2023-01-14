a81cf523bd60a84fc65b1d41ba404822b34d4ab2
Dvin <dvyu@yandex.ru>
2023-01-14 13:43:10 +0500

    entrypoint.py

--- my_git.py
+++ my_git.py
@@ -8,7 +8,8 @@ def git_safe_directory():
 
 # Get changed files list
 def git_diff_files_list(prev_commit, last_commit):
-    git_cmd_diff = "git diff --name-only " + prev_commit + " " + last_commit
+    #git_cmd_diff = "git diff --name-only " + prev_commit + " " + last_commit
+    git_cmd_diff = "git log -3"
     print("git_cmd_diff:", git_cmd_diff)
     cmd_pipe = subprocess.Popen(git_cmd_diff, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     files_list = []
