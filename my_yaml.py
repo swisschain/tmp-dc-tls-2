e9dd1aa35e5f73ec25f12c09fc02396b72ca06f0
Dvin <dvyu@yandex.ru>
2023-01-23 12:01:19 +0500

    entrypoint.py

--- my_yaml.py
+++ my_yaml.py
@@ -8,9 +8,9 @@ def yaml_load(changed_file_name):
         changed_file_yaml = yaml.load(changed_file, Loader=yaml.SafeLoader)
       #except yaml.YAMLError as exc:
       #  print('yaml file parse exception:', exc)
-    if os.getenv('LOG') == 'DEBUG':
-        print('type(changed_file_yaml):', type(changed_file_yaml))
-        print('changed_file_yaml:', changed_file_yaml)
+    #if os.getenv('LOG') == 'DEBUG':
+    #    print('type(changed_file_yaml):', type(changed_file_yaml))
+    #    print('changed_file_yaml:', changed_file_yaml)
     #if isinstance(changed_file_yaml, dict):
     return changed_file_yaml
 
