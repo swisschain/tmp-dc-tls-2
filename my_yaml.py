c386b920705ffbfd56365f3ad86c8f16dfb69424
Dvin <dvyu@yandex.ru>
2023-01-23 12:11:00 +0500

    entrypoint.py

--- my_yaml.py
+++ my_yaml.py
@@ -5,14 +5,14 @@ import yaml
 def yaml_load(changed_file_name):
     with open(changed_file_name, 'r') as changed_file:
       try:
-        changed_file_yaml = yaml.load(changed_file, Loader=yaml.SafeLoader)
+          changed_file_yaml = yaml.load(changed_file, Loader=yaml.SafeLoader)
       #except yaml.YAMLError as exc:
       #  print('yaml file parse exception:', exc)
-    #if os.getenv('LOG') == 'DEBUG':
-    #    print('type(changed_file_yaml):', type(changed_file_yaml))
-    #    print('changed_file_yaml:', changed_file_yaml)
-    #if isinstance(changed_file_yaml, dict):
-    return changed_file_yaml
+      finally:
+          if os.getenv('LOG') == 'DEBUG':
+              print('type(changed_file_yaml):', type(changed_file_yaml))
+              print('changed_file_yaml:', changed_file_yaml)
+          return changed_file_yaml
 
 # Check if yaml path key exist
 def get_yaml_path_key(yaml_file, yaml_path):
