acfbeb36c60e40c624d8e5dce26256cab88695bc
Dvin <dvyu@yandex.ru>
2023-01-25 21:17:45 +0500

    entrypoint.py

--- my_git.py
+++ my_git.py
@@ -27,8 +27,8 @@ def get_git_diff_files_list(prev_commit, last_commit, operations_type):
         git_cmd_diff = "git diff --name-only " + prev_commit + " " + last_commit
     else:
         git_cmd_diff = "git diff --diff-filter=" + git_diff_filter[operations_type] + " --name-only " + prev_commit + " " + last_commit
-    if os.getenv('LOG') == 'DEBUG':
-        print("git_cmd_diff:", git_cmd_diff)
+    #if os.getenv('LOG') == 'DEBUG':
+    print("git_cmd_diff:", git_cmd_diff)
     cmd_pipe = subprocess.Popen(git_cmd_diff, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     files_list = []
     for git_response_line in cmd_pipe.stdout.readlines():
